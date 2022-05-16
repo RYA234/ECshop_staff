0c050221ea74bab3959653d891423f8ce1df8ae3
RYA234 <mryuabc@gmail.com>
2022-05-16 23:08:26 +0900

    CD確認用

--- src/main/java/com/example/demo/Ec2Application.java
+++ src/main/java/com/example/demo/Ec2Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ec2Application {
+public class Ec2Application
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Ec2Application.class, args);
 	}
 
