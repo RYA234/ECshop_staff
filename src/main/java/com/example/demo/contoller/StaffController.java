af0c3b60764ea2e86deb6fa381067420793c8f2b
RYA234 <mryuabc@gmail.com>
2022-05-20 06:42:43 +0900

    Junit5　MVCのプラグイン 環境構築

--- src/main/java/com/example/demo/contoller/StaffController.java
+++ src/main/java/com/example/demo/contoller/StaffController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.testng.annotations.Test;
 
 @Controller
-public class staffController
+public class StaffController
 {
     @RequestMapping(value = MvcStatic.Staff.Add.STAFF_ADD_URL,params = MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD, method = RequestMethod.POST)
     public String postStaffAdd()
