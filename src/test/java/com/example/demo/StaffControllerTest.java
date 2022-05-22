9eabee61c0df7ae21adbe7fe3eae58af3fe10eac
RYA234 <mryuabc@gmail.com>
2022-05-22 11:06:15 +0900

    Junit5:MVCの内ControllerとModelのテストコードを実装

--- src/test/java/com/example/demo/StaffControllerTest.java
+++ src/test/java/com/example/demo/StaffControllerTest.java
@@ -2,22 +2,14 @@ package com.example.demo;
 
 
 import com.example.demo.contoller.IndexController;
-import com.example.demo.contoller.MvcStatic;
-import com.example.demo.contoller.StaffController;
-import org.apache.ibatis.javassist.bytecode.ExceptionsAttribute;
-
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -39,8 +31,8 @@ public class StaffControllerTest
         System.out.println("abbbb");
         MockitoAnnotations.initMocks(this);
         this.loginMvc = MockMvcBuilders.standaloneSetup(testIndexController).build();
-        model().attribute(MvcStatic.Staff.INDEX_TO_STAFF_NAME, MvcStatic.Staff.STAFF_LIST_URL);
-        model().attribute(MvcStatic.Staff.PARAM_INDEX_TO_STAFF_LIST,MvcStatic.Staff.PARAM_INDEX_TO_STAFF_LIST);
+        //model().attribute(MvcStatic.Staff.INDEX_TO_STAFF_NAME, MvcStatic.Staff.STAFF_LIST_URL);
+        //model().attribute(MvcStatic.Staff.PARAM_INDEX_TO_STAFF_LIST,MvcStatic.Staff.PARAM_INDEX_TO_STAFF_LIST);
 
     }
 
@@ -57,5 +49,17 @@ public class StaffControllerTest
                  );
 
     }
+    @Test
+    public void indexのコントローラーModelのテスト() throws Exception
+    {
+        this.loginMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("STAFF_LIST_NAME", "/staff/staff_list")
+                //.andExpect(model().attribute(MvcStatic.Staff.PARAM_INDEX_TO_STAFF_LIST,MvcStatic.Staff.PARAM_INDEX_TO_STAFF_LIST)
+                );
+    }
+
+
 
 }
