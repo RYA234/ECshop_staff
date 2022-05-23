d8ac1a98461b4ae0ef323f081d24461252c4cb64
RYA234 <mryuabc@gmail.com>
2022-05-23 23:36:53 +0900

    スタッフ追加機能-大まかに完成 バリデーションなどは不十分

--- src/main/java/com/example/demo/contoller/StaffController.java
+++ src/main/java/com/example/demo/contoller/StaffController.java
@@ -24,7 +24,6 @@ public class StaffController
    private ModelMapper modelMapper;
 
     @RequestMapping(value = MvcStatic.Staff.Add.STAFF_ADD_URL,params = MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD, method = RequestMethod.POST)
-    //@RequestMapping(value = "staff/staff_add" ,params = MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD, method = RequestMethod.POST)
     public String postStaffListToAdd(Model model)
     {
         System.out.println("bbbv");
@@ -35,7 +34,6 @@ public class StaffController
         model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_ADD_BACK,MvcStatic.Staff.Add.PARAM_STAFF_ADD_BACK);
 
         return MvcStatic.Staff.Add.STAFF_ADD_URL;
-        //return "staff/staff_add";
     }
 
 
@@ -56,28 +54,38 @@ public class StaffController
         model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
         model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
         System.out.println("zzzz");
-       // MStaff staff = modelMapper.map(form, MStaff.class);
+
         List<MStaff> staffList = staffService.getStaffs();
         System.out.println(staffList);
         model.addAttribute("staffList", staffList);
-        System.out.println("yyyy");
-        //return MvcStatic.Staff.STAFF_LIST_URL;
+
         return MvcStatic.Staff.STAFF_LIST_URL;
     }
     @RequestMapping(value = MvcStatic.Staff.Add.STAFF_ADD_CHECK_URL, params = MvcStatic.Staff.Add.PARAM_STAFF_ADD_TO_CHECK, method = RequestMethod.POST)
-    public String postStaffAddToCheck(Model model)
+    public String postStaffAddToCheck(Model model,@ModelAttribute StaffListForm form)
     {
-
+        System.out.println("deffffffsda");
+        System.out.println(form.getName());
+        System.out.println(form.getPassword());
+//        MStaff staff =  MStaff.builder().build();
+//
+//
+//        System.out.println(staff.getPassword());
+        model.addAttribute("name",form.getName());
+        model.addAttribute("password",form.getPassword());
         model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_DONE_NAME,MvcStatic.Staff.Add.STAFF_ADD_DONE_URL);
         model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_CHECK_TO_DONE,MvcStatic.Staff.Add.PARAM_STAFF_CHECK_TO_DONE);
 
         model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
         model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_CHECK_BACK,MvcStatic.Staff.Add.PARAM_STAFF_CHECK_BACK);
 
-        MStaff teststaff = MStaff.builder()
-                .name("zyx")
-                .password("nnn").build();
-        staffService.addStaff(teststaff);
+//        MStaff teststaff = MStaff.builder()
+//                .name("zyx")
+//                .password("nnn").build();
+
+       // MStaff staff = modelMapper.map(form, MStaff.class);
+        //System.out.println(staff);
+        //staffService.addStaff(teststaff);
 
         return MvcStatic.Staff.Add.STAFF_ADD_CHECK_URL;
     }
@@ -88,12 +96,17 @@ public class StaffController
         model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
         model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
 
+
         model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
         model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
 
         model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
         model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
+        System.out.println("zzzz");
 
+        List<MStaff> staffList = staffService.getStaffs();
+        System.out.println(staffList);
+        model.addAttribute("staffList", staffList);
         return MvcStatic.Staff.STAFF_LIST_URL;
     }
 
@@ -109,10 +122,43 @@ public class StaffController
     }
 
     @RequestMapping(value = MvcStatic.Staff.Add.STAFF_ADD_DONE_URL, params = MvcStatic.Staff.Add.PARAM_STAFF_CHECK_TO_DONE, method = RequestMethod.POST)
-    public String postStaffCheckToDone()
+    public String postStaffCheckToDone(Model model,@ModelAttribute StaffListForm form)
     {
-
+        System.out.println("スタッフ追加確認画面からスタッフ追加完了画面に遷移しました。");
+        System.out.println(form.getName());
+        System.out.println(form.getPassword());
+        MStaff staff = MStaff.builder()
+                .name(form.getName())
+                .password(form.getPassword())
+                .build();
+        staffService.addStaff(staff);
+        model.addAttribute(MvcStatic.Staff.STAFF_LIST_NAME, MvcStatic.Staff.STAFF_LIST_URL);
+        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_DONE_BACK, MvcStatic.Staff.Add.PARAM_STAFF_DONE_BACK);
         return MvcStatic.Staff.Add.STAFF_ADD_DONE_URL;
     }
+
+    @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL,params = MvcStatic.Staff.Add.PARAM_STAFF_DONE_BACK,method = RequestMethod.POST)
+    public String postStaffDoneToList(Model model)
+    {
+        System.out.println("タスク追加画面からリストに戻ります。");
+        model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
+        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
+
+
+        model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
+        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
+
+        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
+        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
+        System.out.println("zzzz");
+
+        List<MStaff> staffList = staffService.getStaffs();
+        System.out.println(staffList);
+        model.addAttribute("staffList", staffList);
+        return MvcStatic.Staff.STAFF_LIST_URL;
+    }
+
+
+
 
 }
--- src/main/java/com/example/demo/form/StaffListForm.java
+++ src/main/java/com/example/demo/form/StaffListForm.java
@@ -1,7 +1,11 @@
 package com.example.demo.form;
 
+import lombok.Data;
+
+@Data
 public class StaffListForm
 {
     private Integer id;
     private String name;
+    private String password;
 }
