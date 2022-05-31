f81274ffec1a4567108e09365e4cc5e1704945a5
RYA234 <mryuabc@gmail.com>
2022-05-31 23:39:37 +0900

    Validation追加

--- src/main/java/com/example/demo/contoller/StaffEditController.java
+++ src/main/java/com/example/demo/contoller/StaffEditController.java
@@ -2,11 +2,14 @@ package com.example.demo.contoller;
 
 import com.example.demo.domain.model.MStaff;
 import com.example.demo.form.StaffListForm;
-import com.example.demo.repository.service.StaffService;
+import com.example.demo.service.StaffService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,8 +20,23 @@ public class StaffEditController {
     @Autowired
     private ModelMapper modelMapper;
 
+    /**
+     *スタッフ一覧画面からスタッフ編集画面に遷移するコントローラーです。<br>
+     *
+     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}	<br>
+     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_URL}		<br>
+     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
+     *
+     * @param model Viewに渡す変数
+     * 　　　　　	<p>1.遷移先のURL<br>
+     * 				     2.formアクションの値 </p>
+     *
+     * @return　PARAM_STAFF_LIST_TO_EDIT= {@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_URL}:遷移先URL
+     *注意：ブラウザのURL上ではstaff/staff_listになります。
+     *
+     */
     @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL, params = MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT, method = RequestMethod.POST)
-    public String postStaffListtoEdit(Model model, StaffListForm form) {
+    public String postStaffListtoEdit(Model model,StaffListForm form) {
         model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_CHECK_NAME, MvcStatic.Staff.Edit.STAFF_EDIT_CHECK_URL);
         model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_CHECK_TO_DONE, MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_CHECK_TO_DONE);
 
@@ -26,6 +44,12 @@ public class StaffEditController {
         int selectedId = Integer.valueOf(form.getRadio());
         MStaff selectedStaff = MStaff.builder().build();
         selectedStaff = staffEditService.getStaff(selectedId);
+
+        form.setId(selectedStaff.getId());
+        form.setName(selectedStaff.getName());
+        form.setPassword(selectedStaff.getPassword());
+        model.addAttribute(form);
+
 
         model.addAttribute("id", selectedStaff.getId());
         model.addAttribute("name", selectedStaff.getName());
@@ -35,19 +59,37 @@ public class StaffEditController {
         model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_TO_CHECK, MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_TO_CHECK);
 
 
-        System.out.println();
+        System.out.println(form);
 
 
         return MvcStatic.Staff.Edit.STAFF_EDIT_URL;
 
     }
-
+    /**
+     *スタッフ編集画面からスタッフ編集確認画面に遷移するコントローラーです。<br>
+     *
+     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_URL}	<br>
+     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_CHECK_URL}		<br>
+     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
+     *
+     * @param model Viewに渡す変数
+     * 　　　　　	<p>1.遷移先のURL<br>
+     * 				     2.formアクションの値 </p>
+     *
+     * @return　PARAM_STAFF_EDIT_CHECK_URL= {@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_CHECK_URL}:遷移先URL
+     *
+     *
+     */
     @RequestMapping(value = MvcStatic.Staff.Edit.STAFF_EDIT_CHECK_URL, params = MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_TO_CHECK, method = RequestMethod.POST)
-    public String postStaffEditToCheck(Model model, StaffListForm form) {
+    public String postStaffEditToCheck(Model model, @ModelAttribute @Validated StaffListForm form, BindingResult bindingResult) {
         System.out.println("staff_editからstaff_edit_checkに移動しました");
         System.out.println(form.getName());
         System.out.println(form.getId());
         System.out.println(form.getPassword());
+
+        if(bindingResult.hasErrors()){
+            return MvcStatic.Staff.Edit.STAFF_EDIT_URL;
+        }
 
 
         model.addAttribute("id", form.getId());
@@ -58,6 +100,21 @@ public class StaffEditController {
         return MvcStatic.Staff.Edit.STAFF_EDIT_CHECK_URL;
     }
 
+    /**
+     *スタッフ編集確認画面からスタッフ編集完了画面に遷移するコントローラーです。<br>
+     *
+     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_CHECK_URL}	<br>
+     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_DONE_URL}		<br>
+     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
+     *
+     * @param model Viewに渡す変数
+     * 　　　　　	<p>1.遷移先のURL<br>
+     * 				     2.formアクションの値 </p>
+     *
+ * @return　PARAM_STAFF_EDIT_DONE_URL= {@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_DONE_URL}:遷移先URL
+     *
+     *
+     */
     @RequestMapping(value = MvcStatic.Staff.Edit.STAFF_EDIT_DONE_URL, params = MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_CHECK_TO_DONE, method = RequestMethod.POST)
     public String postStaffEditCheckToDone(Model model, StaffListForm form)
     {
--- src/main/java/com/example/demo/form/StaffListForm.java
+++ src/main/java/com/example/demo/form/StaffListForm.java
@@ -2,11 +2,20 @@ package com.example.demo.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class StaffListForm
 {
+
     private Integer id;
+    @NotEmpty(message="名前を入力してください")
+    @Size(max = 15, message = "名前は15字以内で入力してください")
     private String name;
+
+    @NotEmpty(message="パスワードを入力してください")
+    @Size(max = 15, message = "パスワードは15字以内で入力してください")
     private String password;
     private String radio;
 }
