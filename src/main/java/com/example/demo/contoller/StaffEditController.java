869dadbb35f46760864669c57cff13dcf9fb2a29
RYA234 <mryuabc@gmail.com>
2022-06-01 08:08:54 +0900

    課題：パスワードの入力方法 javadock記入

--- src/main/java/com/example/demo/contoller/StaffEditController.java
+++ src/main/java/com/example/demo/contoller/StaffEditController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class StaffEditController {
     @Autowired
@@ -58,6 +60,8 @@ public class StaffEditController {
         model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME, MvcStatic.Staff.Edit.STAFF_EDIT_URL);
         model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_TO_CHECK, MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_TO_CHECK);
 
+        model.addAttribute(MvcStatic.Staff.STAFF_LIST_NAME,MvcStatic.Staff.STAFF_LIST_URL);
+        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_BACK, MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_BACK);
 
         System.out.println(form);
 
@@ -97,9 +101,46 @@ public class StaffEditController {
         model.addAttribute("password", form.getPassword());
         model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_DONE_NAME, MvcStatic.Staff.Edit.STAFF_EDIT_DONE_URL);
         model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_CHECK_TO_DONE,MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_CHECK_TO_DONE);
+
+
         return MvcStatic.Staff.Edit.STAFF_EDIT_CHECK_URL;
     }
+    /**
+     *スタッフ編集画面からスタッフ一覧画面に戻るコントローラーです。<br>
+     *
+     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_URL}	<br>
+     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}		<br>
+     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
+     *
+     * @param model Viewに渡す変数
+     * 　　　　　	<p>1.遷移先のURL<br>
+     * 				     2.formアクションの値 </p>
+     *
+     * @return　PARAM_STAFF_LIST_URL= {@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}:遷移先URL
+     *
+     *
+     */
+    @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL, params = MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_BACK, method = RequestMethod.POST)
+    public String postStaffEditBack(Model model,StaffListForm form)
+    {
+        System.out.println("スタッフ編集画面からスタッフリストに戻ります。");
+        model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
+        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
 
+
+        model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
+        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
+
+        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
+        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
+        System.out.println("zzzz");
+
+        List<MStaff> staffList = staffEditService.getStaffs();
+        System.out.println(staffList);
+        model.addAttribute("staffList", staffList);
+        return MvcStatic.Staff.STAFF_LIST_URL;
+
+    }
     /**
      *スタッフ編集確認画面からスタッフ編集完了画面に遷移するコントローラーです。<br>
      *
@@ -133,5 +174,75 @@ public class StaffEditController {
         staffEditService.updateStaffone(intId,form.getName(), form.getPassword());
 
         return MvcStatic.Staff.Edit.STAFF_EDIT_DONE_URL;
+    }
+
+    /**
+     *スタッフ編集確認画面からスタッフ編集画面に戻るコントローラーです。<br>
+     *
+     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_CHECK_URL}	<br>
+     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_URL}		<br>
+     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
+     *
+     * @param model Viewに渡す変数
+     * 　　　　　	<p>1.遷移先のURL<br>
+     * 				     2.formアクションの値 </p>
+     *
+     * @return　PARAM_STAFF_EDIT_URL= {@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_URL}:遷移先URL
+     *
+     *
+     */
+    @RequestMapping(value = MvcStatic.Staff.Edit.STAFF_EDIT_URL, params = MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_CHECK_BACK, method = RequestMethod.POST)
+    public String postStaffEditCheckBack(Model model, StaffListForm form)
+    {
+        model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
+        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
+
+
+        model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
+        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
+
+        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
+        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
+
+        List<MStaff> staffList = staffEditService.getStaffs();
+        System.out.println(staffList);
+        model.addAttribute("staffList", staffList);
+        return MvcStatic.Staff.Edit.STAFF_EDIT_URL;
+    }
+
+    /**
+     *スタッフ編集完了画面からスタッフ一覧画面に戻るコントローラーです。<br>
+     *
+     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff.Edit#STAFF_EDIT_DONE_URL}	<br>
+     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}		<br>
+     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
+     *
+     * @param model Viewに渡す変数
+     * 　　　　　	<p>1.遷移先のURL<br>
+     * 				     2.formアクションの値 </p>
+     *
+     * @return　PARAM_STAFF_LIST_URL= {@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}:遷移先URL
+     *
+     *
+     */
+    @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL, params = MvcStatic.Staff.Edit.PARAM_STAFF_EDIT_DONE_BACK method = RequestMethod.POST)
+    public String postStaffEditDoneBack(Model model, StaffListForm form)
+    {
+        System.out.println("タスク編集完了画面からスタッフリスト画面に戻ります。");
+        model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
+        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
+
+
+        model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
+        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
+
+        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
+        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
+        System.out.println("zzzz");
+
+        List<MStaff> staffList = staffEditService.getStaffs();
+        System.out.println(staffList);
+        model.addAttribute("staffList", staffList);
+        return MvcStatic.Staff.STAFF_LIST_URL;
     }
 }
