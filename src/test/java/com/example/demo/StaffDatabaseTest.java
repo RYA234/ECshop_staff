e305666e1b4312ad6e2b1b47a811af2fcc399d65
RYA234 <mryuabc@gmail.com>
2022-06-05 18:16:30 +0900

    スタッフ機能：参照削除　テストコード記入

--- src/test/java/com/example/demo/StaffDatabaseTest.java
+++ src/test/java/com/example/demo/StaffDatabaseTest.java
@@ -1,55 +1,103 @@
-//package com.example.demo;
-//
-//import com.example.demo.domain.model.MStaff;
-//import com.example.demo.service.StaffService;
-//import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-//import com.github.springtestdbunit.annotation.DatabaseSetup;
-//import com.github.springtestdbunit.annotation.DbUnitConfiguration;
-//import com.github.springtestdbunit.annotation.ExpectedDatabase;
-//import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestExecutionListeners;
-//import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//
-//@SpringBootTest
-//@DbUnitConfiguration(dataSetLoader = CsvDatasSetLoader.class) // DBUnitでCSVファイルを使えるよう指定。＊CsvDataSetLoaderクラスは自作します（後述）
-//@TestExecutionListeners({
-//        DependencyInjectionTestExecutionListener.class, // このテストクラスでDIを使えるように指定
-//        TransactionDbUnitTestExecutionListener.class // @DatabaseSetupや＠ExpectedDatabaseなどを使えるように指定
-//})
-//@Transactional // @DatabaseSetupで投入するデータをテスト処理と同じトランザクション制御とする。（テスト後に投入データもロールバックできるように）
-//public class StaffDatabaseTest
-//{
-//    @Autowired
-//    private StaffService staffService;
-//
-//    @Test
-//    @DatabaseSetup("/testdata/StaffServiceTest/init-data")
-//    @ExpectedDatabase(value = "/testdata/StaffServiceTest/after-create-data", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED) // テスト実行後に１件データが追加されていること
-//    void データベーススタッフが追加されるか確認()
-//    {
-//        MStaff newStaff = MStaff.builder()
-//                .id(4)
-//                .name("zyx")
-//                .password("nnn").build();
-//
-//        staffService.addStaff(newStaff);
-//        Assertions.assertEquals(4,newStaff.getId());
-//        Assertions.assertEquals("zyx",newStaff.getName());
-//        Assertions.assertEquals("nnn",newStaff.getPassword());
-//    }
-//
-//    @Test
-//    @DatabaseSetup("/testdata/StaffServiceTest/init-data")
-//    @ExpectedDatabase(value = "/testdata/StaffServiceTest/after-delete-data", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED) // テスト実行後に１件データが追加されていること
-//    void データベーススタッフが削除されるか確認()
-//    {
-//        staffService.deleteStaff(2);
-//
-//    }
-//}
+package com.example.demo;
+
+import com.example.demo.domain.model.MStaff;
+import com.example.demo.service.StaffService;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@SpringBootTest
+@DbUnitConfiguration(dataSetLoader = CsvDatasSetLoader.class) // DBUnitでCSVファイルを使えるよう指定。＊CsvDataSetLoaderクラスは自作します（後述）
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class, // このテストクラスでDIを使えるように指定
+        TransactionDbUnitTestExecutionListener.class // @DatabaseSetupや＠ExpectedDatabaseなどを使えるように指定
+})
+@Transactional // @DatabaseSetupで投入するデータをテスト処理と同じトランザクション制御とする。（テスト後に投入データもロールバックできるように）
+public class StaffDatabaseTest
+{
+    @Autowired
+    private StaffService staffService;
+
+    @Test
+    @DisplayName("データベーススタッフが追加されるか確認")
+    @DatabaseSetup("/testdata/StaffServiceTest/init-data")
+    @ExpectedDatabase(value = "/testdata/StaffServiceTest/after-create-data", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED) // テスト実行後に１件データが追加されていること
+    void staffAddCheck()
+    {
+        MStaff newStaff = MStaff.builder()
+                .id(3)
+                .name("zyx")
+                .password("nnn").build();
+
+        staffService.addStaff(newStaff);
+        Assertions.assertEquals(3,newStaff.getId());
+        Assertions.assertEquals("zyx",newStaff.getName());
+        Assertions.assertEquals("nnn",newStaff.getPassword());
+    }
+
+
+    @Test
+    @DisplayName("データベーススタッフが更新されるか確認")
+    @DatabaseSetup("/testdata/StaffServiceTest/init-data")
+    @ExpectedDatabase(value = "/testdata/StaffServiceTest/after-update-data", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED) // テスト実行後に１件データが追加されていること
+    void staffUpdate()
+    {
+
+        staffService.updateStaffone(2,"oooo","iiiii");
+
+        Assertions.assertEquals(2,2);
+        Assertions.assertEquals("oooo","oooo");
+        Assertions.assertEquals("iiiii","iiiii");
+
+    }
+
+    @Test
+    @DisplayName("データベーススタッフが一件選択されるか確認")
+    @DatabaseSetup("/testdata/StaffServiceTest/init-data")
+    @ExpectedDatabase(value = "/testdata/StaffServiceTest/init-data", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED) // テスト実行後に１件データが追加されていること
+    void staffSelectOne()
+    {
+        MStaff staff = staffService.getStaff(2);
+//        2,ffgsd,dfaas
+        Assertions.assertEquals(2,staff.getId());
+        Assertions.assertEquals("ffgsd",staff.getName());
+        Assertions.assertEquals("dfaas",staff.getPassword());
+
+    }
+
+    @Test
+    @DisplayName("データベーススタッフが全件選択されるか確認")
+    @DatabaseSetup("/testdata/StaffServiceTest/init-data")
+    @ExpectedDatabase(value = "/testdata/StaffServiceTest/init-data", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED) // テスト実行後に１件データが追加されていること
+    void staffSelectAll()
+    {
+        List<MStaff> staff = staffService.getStaffs();
+
+        Assertions.assertEquals("abcd", staff.get(0).getName());
+        Assertions.assertEquals("zzz", staff.get(0).getPassword());
+        Assertions.assertEquals("ffgsd", staff.get(1).getName());
+        Assertions.assertEquals("dfaas", staff.get(1).getPassword());
+    }
+
+
+
+
+
+
+
+
+
+}
