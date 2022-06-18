f72fccb2cb40a3d9a91f6fbf2d0c52a685038836
Maruyama Kai <0123mark3210@gmail.com>
2022-06-18 18:12:59 +0900

    ECShopMapper Interfaceのファイルをフォーマット

--- src/main/java/com/example/demo/repository/ECShopMapper.java
+++ src/main/java/com/example/demo/repository/ECShopMapper.java
@@ -8,26 +8,29 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 @Mapper
-public interface ECShopMapper
-{
+public interface ECShopMapper {
     List<MStaff> staffFindMany();
+
     void staffInsertOne(MStaff staff);
+
     MStaff staffFindOne(Integer id);
+
     void staffUpdate(@Param("id") Integer id,
-                            @Param("name") String name,
-                            @Param("password") String password);
+                     @Param("name") String name,
+                     @Param("password") String password);
 
     void staffDelete(Integer id);
 
     List<MProduct> productFindMany();
+
     void productInsertOne(MProduct product);
+
     MProduct productFindOne(Integer code);
+
     void productUpdate(@Param("code") Integer code,
-                            @Param("name") String name,
-                            @Param("price") Integer price,
-                            @Param("gazou") String gazou);
+                       @Param("name") String name,
+                       @Param("price") Integer price,
+                       @Param("gazou") String gazou);
 
     void productDelete(Integer code);
-    
-
 }
