8be78aac96c6bbd4d75f106d29e67dc6a2e3ee27
RYA234 <mryuabc@gmail.com>
2022-07-18 22:32:05 +0900

    スタッフ　削除画面　リファクタリング完了 model.addAttributeの削除

--- src/main/java/com/example/demo/contoller/StaffDeleteController.java
+++ src/main/java/com/example/demo/contoller/StaffDeleteController.java
@@ -7,7 +7,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,26 +35,16 @@ public class StaffDeleteController {
      *
      *
      */
-    @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL, params = MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE, method = RequestMethod.POST)
-    public String postStaffListtoDelete(Model model, StaffListForm form)
+    @RequestMapping(value = "staff/staff_list", params = "PARAM_STAFF_LIST_TO_DELETE", method = RequestMethod.POST)
+    public String postStaffListToDelete(Model model, StaffListForm form)
     {
         System.out.println("スタッフ一覧画面からスタッフ削除画面に遷移します");
-        System.out.println(form.getRadio());
         int selectedId = Integer.valueOf(form.getRadio());
         MStaff selectedStaff = MStaff.builder().build();
         selectedStaff = staffDeleteService.getStaff(selectedId);
         model.addAttribute("name", selectedStaff.getName());
         model.addAttribute("id",selectedStaff.getId());
-
-        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_DONE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_DONE_URL);
-        model.addAttribute(MvcStatic.Staff.STAFF_LIST_NAME,MvcStatic.Staff.STAFF_LIST_URL);
-        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_TO_DONE,MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_TO_DONE);
-
-        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_BACK,MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_BACK);
-//        model.addAttribute();
-//        model.addAttribute();
-
-        return MvcStatic.Staff.Delete.STAFF_DELETE_URL;
+        return "staff/staff_delete";
     }
 
 
@@ -74,18 +63,12 @@ public class StaffDeleteController {
      *
      *
      */
-    @RequestMapping(value = MvcStatic.Staff.Delete.STAFF_DELETE_DONE_URL, params = MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_TO_DONE, method = RequestMethod.POST)
+    @RequestMapping(value = "staff/staff_delete_done", params = "PARAM_STAFF_DELETE_TO_DONE", method = RequestMethod.POST)
     public String postStaffDeleteToDone(Model model, StaffListForm form)
     {
         System.out.println("スタッフ削除画面からスタッフ削除完了画面に遷移します");
-
-        System.out.println(form.getId());
         staffDeleteService.deleteStaffone(form.getId());
-
-        model.addAttribute(MvcStatic.Staff.STAFF_LIST_NAME,MvcStatic.Staff.STAFF_LIST_URL);
-        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_DONE_BACK,MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_DONE_BACK);
-
-        return MvcStatic.Staff.Delete.STAFF_DELETE_DONE_URL;
+        return "staff/staff_delete_done";
     }
 
     /**
@@ -103,68 +86,14 @@ public class StaffDeleteController {
      *
      *
      */
-    @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL, params = MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_BACK, method = RequestMethod.POST)
+    @RequestMapping(value = "staff/staff_list", params = "PARAM_STAFF_DELETE_BACK", method = RequestMethod.POST)
     public String postStaffDeleteBack(Model model, StaffListForm form)
     {
-        System.out.println("スタッフ削除画面からスタッフ一覧画面に遷移します");
-
-        System.out.println("スタッフ編集画面からスタッフリストに戻ります。");
-        model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
-        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
-
-
-        model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
-        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
-
-        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
-        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
-        System.out.println("zzzz");
-
+        System.out.println("スタッフ削除or完了画面からスタッフ一覧画面に遷移します");
         List<MStaff> staffList = staffDeleteService.getStaffs();
-        System.out.println(staffList);
+//        System.out.println(staffList);
         model.addAttribute("staffList", staffList);
-
-        return MvcStatic.Staff.STAFF_LIST_URL;
-    }
-
-
-
-    /**
-     *スタッフ削除完了画面からスタッフ一覧画面に遷移するコントローラーです。<br>
-     *
-     * 遷移前URL：{@value com.example.demo.contoller.MvcStatic.Staff.Delete#STAFF_DELETE_DONE_URL}	<br>
-     * 遷移後URL：{@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}		<br>
-     *　html側のURLとform actionの変数はthymeleafを使ってJavaの変数から渡しています。
-     *
-     * @param model Viewに渡す変数
-     * 　　　　　	<p>1.遷移先のURL<br>
-     * 				     2.formアクションの値 </p>
-     *
-     * @return　STAFF_LIST_URL= {@value com.example.demo.contoller.MvcStatic.Staff#STAFF_LIST_URL}:遷移先URL
-     *
-     *
-     */
-    @RequestMapping(value = MvcStatic.Staff.STAFF_LIST_URL, params = MvcStatic.Staff.Delete.PARAM_STAFF_DELETE_DONE_BACK, method = RequestMethod.POST)
-    public String postStaffDeleteDoneBack(Model model,@ModelAttribute StaffListForm form)
-    {
-        System.out.println("スタッフ削除確認画面からスタッフ一覧画面に遷移します");
-
-        model.addAttribute(MvcStatic.Staff.Add.STAFF_ADD_NAME,MvcStatic.Staff.Add.STAFF_ADD_URL);
-        model.addAttribute(MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD,MvcStatic.Staff.Add.PARAM_STAFF_LIST_TO_ADD);
-
-
-        model.addAttribute(MvcStatic.Staff.Edit.STAFF_EDIT_NAME,MvcStatic.Staff.Edit.STAFF_EDIT_URL);
-        model.addAttribute(MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT,MvcStatic.Staff.Edit.PARAM_STAFF_LIST_TO_EDIT);
-
-        model.addAttribute(MvcStatic.Staff.Delete.STAFF_DELETE_NAME,MvcStatic.Staff.Delete.STAFF_DELETE_URL);
-        model.addAttribute(MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE,MvcStatic.Staff.Delete.PARAM_STAFF_LIST_TO_DELETE);
-        System.out.println("zzzz");
-
-        List<MStaff> staffList = staffDeleteService.getStaffs();
-        System.out.println(staffList);
-        model.addAttribute("staffList", staffList);
-
-        return MvcStatic.Staff.STAFF_LIST_URL;
+        return "staff/staff_list";
     }
 
 
